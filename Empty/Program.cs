46062012209d7a570ce31f389a9bbde593c0950a
GENRAY1 <genray@ro.ru>
2024-03-04 14:23:12 +0300

    middleware & send html files

--- Empty/Program.cs
+++ Empty/Program.cs
@@ -9,45 +9,21 @@ class Program
         app.Run();
     }
     //middleware 
-    static async Task MyMiddleware(HttpContext context) { 
-        var path = context.Request.Path;
-        context.Response.ContentType = "text/html; charset=utf-8";
+    static async Task MyMiddleware(HttpContext context) {
+        var response = context.Response;
+        var request = context.Request;
 
-        switch (path)
+        var path = request.Path;
+        var pagePath = $"static/pages/{path}.html";
+        response.ContentType = "text/html; charset=utf-8";
+
+        if (File.Exists(pagePath))
         {
-            case "/":
-                await context.Response.WriteAsync("<h1>Home page</h1>");
-                break;
-            case "/about":
-                await context.Response.WriteAsync("<h1>About page</h1>");
-                break;
-            /*
-            case "/user":
-                var htmlResponse = new System.Text.StringBuilder("<h1>User page</h1><h3>Параметры строки запроса</h3><table>");
-                htmlResponse.Append("<tr><td>Параметр</td><td>Значение</td></tr>");
-                foreach (var param in context.Request.Query)
-                {
-                    htmlResponse.Append($"<tr><td>{param.Key}</td><td>{param.Value}</td></tr>");
-                }
-                htmlResponse.Append("</table>");
-                await context.Response.WriteAsync(htmlResponse.ToString());
-                break;
-            */
-            case "/user":
-                //https://localhost:7078/user?username=Genray&login=admin&pass=1234
-                string? username = context.Request.Query["username"];
-                string? login = context.Request.Query["login"];
-                string? password = context.Request.Query["pass"];
-
-                await context.Response.WriteAsync($"<h1>Пользователь {username}</h2>" +
-                    $"<div>login: {login}</div>" +
-                    $"<div>password: {password}</div>"
-                    ); 
-                break;
-            default:
-                context.Response.StatusCode = 404;
-                await context.Response.WriteAsync("<h1>Not found</h1> <div>Error 404</div>");
-                break;
+            await response.SendFileAsync(pagePath);
+        }
+        else {
+            var defaultPath = "static/pages/notfound.html";
+            await response.SendFileAsync(defaultPath);
         }
        
     }
